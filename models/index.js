163049c49dbe27c861a6168e80107b04b8757292
Mohammad Nabi Noorzai <mnoorzai21@gmail.com>
2022-05-22 04:30:02 -0700

    Update index.js files in models, routes, routes/api

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const Student = require("./User");
+const Course = require("./Thought");
+
+// Not sure about Thought or Reaction
+const Course = require("./Reaction");
+
+module.exports = { User, Thought };
---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const courseRoutes = require("./thoughtsRoutes");
+const studentRoutes = require("./usersRoutes");
+
+router.use("/thoughts", thoughtsRoutes);
+router.use("/users", usersRoutes);
+
+module.exports = router;
