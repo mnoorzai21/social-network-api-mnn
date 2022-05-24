1cdf6d2a13d05b7926a03df8f6eafffd7d90bd54
Mohammad Nabi Noorzai <mnoorzai21@gmail.com>
2022-05-24 08:22:14 -0700

    Get all users, get a single user, and create user

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,3 +5,44 @@ const headCount = async() =>
     Student.aggregate()
     .count("studentCount")
     .then((numberOfStudents) => numberOfStudents);
+
+module.exports = {
+    // Get all users
+    getUsers(req, res) {
+        User.find()
+            .then(async(users) => {
+                const userObj = {
+                    users,
+                    headCount: await headCount(),
+                };
+                return res.json(userObj);
+            })
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
+    // Get a single user
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+            .select("-__v")
+            .then(async(user) =>
+                !user ?
+                res.status(404).json({ message: "No user with that ID" }) :
+                res.json({
+                    user,
+                    grade: await grade(req.params.userId),
+                })
+            )
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
+    // create a new user
+    createUser(req, res) {
+        User.create(req.body)
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
+    },
+};
