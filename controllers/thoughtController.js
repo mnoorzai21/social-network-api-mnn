5c05702fb0cbc3a83821767d762024e83304f706
Mohammad Nabi Noorzai <mnoorzai21@gmail.com>
2022-05-24 07:24:02 -0700

    getThoughts, getSingleThought, createTought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,1 +1,49 @@
-const { User, Reaction } = require("../models");
+const { User, Thought } = require("../models");
+
+// Need to fixt all these
+
+module.exports = {
+    // Get all thoughts
+    getThoughts(req, res) {
+        Thought.find()
+            .then((thought) => res.json(thought))
+            .catch((err) => res.status(500).json(err));
+    },
+    // Get a thought
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+            .select("-__v")
+            .then((thought) =>
+                !thought ?
+                res.status(404).json({ message: "No thought with that ID" }) :
+                res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    // Create a thought
+    createThought(req, res) {
+        Thought.create(req.body)
+            .then((thought) => {
+                // Push the thought to user's thoughts list
+                return User.findOneAndUpdate({ _id: req.body.userId },
+                    // $addToSet adds a value to an array unless the value is already present
+                    { $addToSet: { thoughts: thought._id } },
+                    // new: true show the changes
+                    { new: true }
+                );
+            })
+            .then((user) => {
+                if (!user) {
+                    res.status(404).json({
+                        message: "Thought is created! But user with the provided Id does not exist!",
+                    });
+                    return;
+                }
+                res.status(200).json("Thought has been created!");
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+};
