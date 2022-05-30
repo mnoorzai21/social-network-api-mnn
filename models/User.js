76b6240b5391789770e08c6ac1ceb36b68c64093
Mohammad Nabi Noorzai <mnoorzai21@gmail.com>
2022-05-29 19:03:26 -0700

    debugged create user's duplicated ids

--- models/User.js
+++ models/User.js
@@ -1,40 +1,48 @@
 const { Schema, model } = require("mongoose");
 
 // Schema to create User model
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     username: {
-        type: String,
-        unique: true,
-        required: true,
-        max_length: 50,
-        trim: true,
+      type: String,
+      unique: true,
+      required: true,
+      max_length: 50,
+      trim: true,
     },
     email: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true,
-        lowercase: true,
-        required: "Email address is required",
-        match: [
-            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-            "Please fill a valid email address",
-        ],
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
+      required: "Email address is required",
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
     },
 
-    friends: [{
+    friends: [
+      {
         type: Schema.Types.ObjectId,
         ref: "User",
-    }, ],
-    thoughts: [{
+      },
+    ],
+    thoughts: [
+      {
         type: Schema.Types.ObjectId,
         ref: "Thought",
-    }, ],
-}, {
+      },
+    ],
+  },
+  {
     toJSON: {
-        getters: true,
+      virtuals: true,
     },
-});
+    id: false,
+  }
+);
 
 const User = model("User", userSchema);
 
